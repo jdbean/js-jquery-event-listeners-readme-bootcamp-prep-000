9b066c7c1cadbbec17f77fb58db0bec2713c7f50
J.D. Bean <jonathandbean@gmail.com>
2017-12-07 23:30:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,9 @@ function pressIt() {
 }
 
 function submitIt() {
-
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  })
 }
 
 $(document).ready(function(){
@@ -30,4 +32,5 @@ $(document).ready(function(){
 getIt();
 frameIt();
 pressIt();
+submitIt();
 });
